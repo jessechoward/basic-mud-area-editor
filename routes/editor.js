b88723ae3b387d67424f59c40dba2a6b848fa9f1
Jesse Howard <jessechoward@gmail.com>
2018-10-25 18:40:15 -0500

    designing routes

--- routes/editor.js
+++ routes/editor.js
@@ -1,9 +1,18 @@
 // eslint-disable-next-line new-cap
 const router = require('express').Router();
 
-router.get('/:editorName', (req, res) =>
-{
-	res.render('editors/'+req.params.editorName);
-});
+router.get('/areas');
+router.get('/areas/:areaId');
+router.get('/areas/:areaId/rooms');
+router.get('/rooms/:roomId');
+router.get('/rooms/:roomId/exits');
+router.get('/exits/:exitId');
+router.get('/areas/:areaId/items');
+router.get('/items/:itemId');
+router.get('/areas/:areaId/npcs');
+router.get('/npcs/:npcId');
+router.get('/areas/:areaId/instances');
+router.get('/instances/:instanceId');
+
 
 module.exports = router;
